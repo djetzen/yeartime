4a252abbc7471067b2ae7f60076e0cdee51212a3
DJetzen <djetzen@users.noreply.github.com>
2021-01-14 23:27:30 +0100

    add use cases and ports

---
+++ src/main/kotlin/de/djetzen/yeartime/application/port/in/ReadSavedDataUseCase.kt
@@ -0,0 +1,10 @@
+package de.djetzen.yeartime.application.port.`in`
+
+import de.djetzen.yeartime.domain.models.Day
+import java.time.LocalDate
+import java.time.Year
+
+interface ReadSavedDataUseCase {
+    fun readDayForUser(day: LocalDate, user: String): Day
+    fun readYearForUser(year: Year, user: String): List<Day>
+}
